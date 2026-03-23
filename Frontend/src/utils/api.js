b6ee76df14d2b2e5f31a8a37421e23cf50ad8534
Aman Bam <amanbam604@gmail.com>
2026-03-23 15:06:47 +0530

    Fix API URL to include /api prefix

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL ='https://employee-management-system-1-b1ii.onrender.com';
+const BASE_URL ='https://employee-management-system-1-b1ii.onrender.com/api';
 
 const getToken = () => localStorage.getItem('token');
 
