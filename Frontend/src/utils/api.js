6135ad508525134c58f1b4de088949ae8d9de211
Aman Bam <amanbam604@gmail.com>
2026-03-23 20:48:46 +0530

    Done

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL ='https://employee-management-system-1-b1ii.onrender.com/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 const getToken = () => localStorage.getItem('token');
 
